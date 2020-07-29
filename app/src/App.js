aa4c4fa2fa2e54200dc6d874f4e28317588815ce
shelleychenn <shelleychen322@gmail.com>
2020-07-29 18:50:17 -0400

    Add component files

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import Explore from './components/Explore';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'explore',
+    };
+  }
+
+  renderView() {
+    const { view } = this.state;
+
+    if (view === 'explore') {
+      return <Explore />;
+    } else if (view === 'favorites') {
+      return <Favorites />;
+    } else if (view === 'profile') {
+      return <Profile />;
+    }
+  }
+
   render() {
-    const { name } = this.props;
     return (
-      <>
-        <h1>Hello {name}</h1>
-      </>
+      <div className="app">
+        <h1>Hello </h1>
+      </div>
     );
   }
 }
--- app/src/index.js
+++ app/src/index.js
@@ -4,4 +4,4 @@ import App from './App';
 import './styles.scss';
 
 var mountNode = document.getElementById('app');
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(<App />, mountNode);
