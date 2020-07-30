2416c1a22867bd0934538a062d1278140d005842
shelleychenn <shelleychen322@gmail.com>
2020-07-30 02:27:22 -0400

    Add structure and basic css to explore page

--- app/src/components/Attraction.js
+++ app/src/components/Attraction.js
@@ -2,27 +2,23 @@ import React from 'react';
 
 const Attraction = ({ attraction }) => {
   if (attraction.result_type === 'geos' && attraction.is_top_result === true) {
-    return (
-      <div className="attraction-item-location">
-        <div>{attraction.result_object.name}</div>
-        <div>{attraction.result_object.num_reviews}</div>
-        <div>{attraction.result_object.photo.images.small.url}</div>
-        <div>{attraction.result_object.ploaded_date}</div>
-        <div>{attraction.geo_description}</div>
-      </div>
-    );
+    return null;
   } else if (attraction.result_type !== 'geos') {
     return (
       <div className="attraction-item">
-        <div>{attraction.result_type}</div>
-        <div>{attraction.result_object.name}</div>
-        <div>{attraction.result_object.num_reviews}</div>
+        <div className="attraction-name">{attraction.result_object.name}</div>
+        <div>
+          <img src={attraction.result_object.photo.images.small.url}></img>
+        </div>
         <div>{attraction.result_object.location_string}</div>
-        <div>{attraction.result_object.photo.images.small.url}</div>
-        <div>{attraction.result_object.caption}</div>
+        <div>{attraction.result_object.num_reviews}</div>
+        <div className="attraction-caption">
+          {attraction.result_object.caption}
+        </div>
         <div>{attraction.result_object.photo.uploaded_date}</div>
         <div>{attraction.result_object.address}</div>
         <div>{attraction.review_snippet.snippet}</div>
+        <div>{attraction.result_type}</div>
       </div>
     );
   } else {
--- app/src/components/AttractionList.js
+++ app/src/components/AttractionList.js
@@ -2,12 +2,48 @@ import React from 'react';
 import Attraction from './Attraction';
 
 const AttractionList = ({ attractions }) => {
+  let locationOverview;
+  if (
+    attractions &&
+    attractions[0] &&
+    attractions[0].result_type === 'geos' &&
+    attractions[0].is_top_result === true
+  ) {
+    locationOverview = (
+      <div>
+        <div className="attraction-name">
+          {attractions[0].result_object.name}
+        </div>
+        <div className="location-images">
+          <div>
+            <img
+              src={attractions[0].result_object.photo.images.small.url}
+            ></img>
+          </div>
+          <div>
+            <img
+              className="location-map"
+              src={attractions[0].result_object.map_image_url}
+            ></img>
+          </div>
+        </div>
+        <div>{attractions[0].result_object.num_reviews}</div>
+        <div>{attractions[0].result_object.geo_description}</div>
+      </div>
+    );
+  } else {
+    locationOverview = null;
+  }
+  console.log('here', attractions);
   return (
-    <div className="attraction-list">
-      {attractions.map((attraction, index) => (
-        <Attraction attraction={attraction} key={index} />
-      ))}
-    </div>
+    <>
+      <div className="attraction-item-location">{locationOverview}</div>
+      <div className="attraction-list">
+        {attractions.slice(1).map((attraction, index) => (
+          <Attraction attraction={attraction} key={index} />
+        ))}
+      </div>
+    </>
   );
 };
 
