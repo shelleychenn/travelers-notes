2a7bc0be8191b534c7a474bc6696f0218ee17632
shelleychenn <shelleychen322@gmail.com>
2020-07-29 20:34:52 -0400

    Set up attraction schema

---
+++ app/db/Attraction.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const db = require('./mongodb');
+mongoose.Promise = global.Promise;
+
+const attractionSchema = new mongoose.Schema({
+  result_type: String,
+  location_id: String,
+  name: String,
+  latitude: String,
+  longitude: String,
+  num_reviews: String,
+  timezone: String,
+  location_string: String,
+  photo: Array,
+  awards: Array,
+  rating: String,
+  description: String,
+  nearest_metro_station: String,
+  geo_description: String,
+  address: String,
+  review_snippet: String,
+});
+
+const AttractionEntry = mongoose.model('AttractionEntry', attractionSchema);
+
+module.exports = AttractionEntry;
--- app/server/index.js
+++ app/server/index.js
@@ -1,1 +1,12 @@
-const Express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
