e28cdc866ea394b54a6d2b5eb65385fe9de2f4c9
shelleychenn <shelleychen322@gmail.com>
2020-07-30 23:38:37 -0400

    Structure and style favoritesListItem card

--- app/src/components/FavoritesListItem.js
+++ app/src/components/FavoritesListItem.js
@@ -3,13 +3,15 @@ import React from 'react';
 const FavoritesListItem = ({ favoritesListItem }) => {
   return (
     <div className="favorites-list-item">
-      <div>{favoritesListItem.name}</div>
-      <div>
+      <div className="favorites-list-item-image">
         <img src={favoritesListItem.image_url}></img>
       </div>
-      <div>{favoritesListItem.location}</div>
-      <div>{favoritesListItem.address}</div>
-      <div>{favoritesListItem.num_of_reviews}</div>
+      <div className="favorites-list-item-info">
+        <div className="favorites-list-item-name">{favoritesListItem.name}</div>
+        <div>{favoritesListItem.location}</div>
+        <div>{favoritesListItem.address}</div>
+        <div>Reviews: {favoritesListItem.num_of_reviews}</div>
+      </div>
     </div>
   );
 };
